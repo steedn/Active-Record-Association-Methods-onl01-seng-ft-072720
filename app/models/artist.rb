c3635cf2536ec44cc40450c02ddbb410e571d0c3
Nathan Steed <steedn@outlook.com>
2020-09-09 04:56:04 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,15 +3,14 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    self.songs.first
+    self.songs.first.genre
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    self.songs.size
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    self.genres.size
   end
 end
-  
